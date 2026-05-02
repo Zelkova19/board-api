9e6008b96583a1b0eae8e775e6579621347c6151
zelkova <cohiba666@gmail.com>
2026-05-02 19:02:09 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 
 from app.post.routes import router as post_router
-from app.rand.routes import router as rand_router
 
 app = FastAPI()
 
 app.include_router(post_router)
-app.include_router(rand_router)
